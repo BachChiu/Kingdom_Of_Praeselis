81eaf159dee85a16d622da9ce8561747faae118f
Bach Chiu <87551553+BachChiu@users.noreply.github.com>
2023-08-30 22:08:43 -0400

    Add files via upload
    Uploaded the modbuddy template on accident, but this one is that same fix for some localisation typo

--- KOP/Leader/Leader_Localisation.sql
+++ KOP/Leader/Leader_Localisation.sql
@@ -24,24 +24,29 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_FIRST_MEET_VISIT_RECIPIENT_LEADER_BC_DUDEOBAMA_ANY',			),
 		--Accepts human invitation
 		('en_US',	'LOC_DIPLO_FIRST_MEET_NEAR_INITIATOR_POSITIVE_LEADER_BC_DUDEOBAMA_ANY',			),
+
 		--Exchange Capital Info
 		('en_US',	'LOC_DIPLO_FIRST_MEET_NO_MANS_INFO_EXCHANGE_LEADER_BC_DUDEOBAMA_ANY',			),
+
 		--Happy Greet
 		('en_US',	'LOC_DIPLO_GREETING_LEADER_BC_DUDEOBAMA_HAPPY',				),
 		--Unhappy Greet
 		('en_US',	'LOC_DIPLO_GREETING_LEADER_BC_DUDEOBAMA_UNHAPPY',			),
+
 		--Accepting delegation
 		('en_US',	'LOC_DIPLO_ACCEPT_DELEGATION_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',		),
 		--Rejecting delegation
 		('en_US',	'LOC_DIPLO_REJECT_DELEGATION_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',			),
 		--Requesting delegation
 		('en_US',	'LOC_DIPLO_DELEGATION_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',					),
+
 		-- Open Borders Accept
 		('en_US',	'LOC_DIPLO_ACCEPT_OPEN_BORDERS_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',					),
 		--Open borders Reject
 		('en_US',	'LOC_DIPLO_REJECT_OPEN_BORDERS_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',				),
 		-- Open border Request
 		('en_US',	'LOC_DIPLO_OPEN_BORDERS_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',				),
+
 		--Friendship accepts from human
 		('en_US',	'LOC_DIPLO_ACCEPT_DECLARE_FRIEND_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',			),
 		--Friendship rejected from human
@@ -52,12 +57,15 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_ACCEPT_DECLARE_FRIEND_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',				),
 		--Human rejects request, Ai responds
 		('en_US',	'LOC_DIPLO_REJECT_DECLARE_FRIEND_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',				),
+
 		--Alliance request
 		('en_US',	'LOC_DIPLO_MAKE_ALLIANCE_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',				),
+
 		-- AI Kudos
 		('en_US',	'LOC_DIPLO_KUDO_EXIT_LEADER_BC_DUDEOBAMA_ANY',				),
 		-- AI Warnings
 		('en_US',	'LOC_DIPLO_WARNING_EXIT_LEADER_BC_DUDEOBAMA_ANY',				),
+
 		-- AI warns player for border troops (HAPPY)
 		('en_US',	'LOC_DIPLO_WARNING_TOO_MANY_TROOPS_NEAR_ME_LEADER_BC_DUDEOBAMA_HAPPY',				),
 		-- AI warns player for border troops (UNHAPPY)
@@ -68,7 +76,8 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_WARNING_TOO_MANY_TROOPS_NEAR_ME_AI_RESPONSE_POSITIVE_LEADER_BC_DUDEOBAMA_UNHAPPY',			),
 		-- AI rejects player's warning for border troops warning (ANY) ~ DECLARES WAR
 		('en_US',	'LOC_DIPLO_WARNING_TOO_MANY_TROOPS_NEAR_ME_AI_RESPONSE_NEGATIVE_LEADER_BC_DUDEOBAMA_ANY',			),
-		-- Diplomacy Text: Settling Too Close
+
+-- Diplomacy Text: Settling Too Close
 		-- AI warns player for settling too close (HAPPY)
 		('en_US',	'LOC_DIPLO_WARNING_DONT_SETTLE_NEAR_ME_AI_LEADER_BC_DUDEOBAMA_HAPPY',			),
 		-- AI warns player for settling too close (UNHAPPY)
@@ -81,6 +90,7 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_WARNING_DONT_SETTLE_NEAR_ME_AI_RESPONSE_NEGATIVE_LEADER_BC_DUDEOBAMA_HAPPY',				),
 		-- Negative AI response to human request (UNHAPPY)
 		('en_US',	'LOC_DIPLO_WARNING_DONT_SETTLE_NEAR_ME_AI_RESPONSE_NEGATIVE_LEADER_BC_DUDEOBAMA_UNHAPPY',			),
+
 		-- AI Accepts Deal (HAPPY)
 		('en_US',	'LOC_DIPLO_ACCEPT_MAKE_DEAL_FROM_AI_LEADER_BC_DUDEOBAMA_HAPPY',				),
 		-- AI Accepts Deal (Unhappy)
@@ -89,6 +99,7 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_REJECT_MAKE_DEAL_FROM_AI_LEADER_BC_DUDEOBAMA_HAPPY',				),
 		-- AI Rejects Deal (Unhappy)
 		('en_US',	'LOC_DIPLO_REJECT_MAKE_DEAL_FROM_AI_LEADER_BC_DUDEOBAMA_UNHAPPY',			),
+
 		-- Human denounce, AI responds
 		('en_US',	'LOC_DIPLO_DENOUNCE_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',			),
 		-- AI Denounce
@@ -97,7 +108,7 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_DECLARE_WAR_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY',				),
 		-- AI Declares War
 		('en_US',	'LOC_DIPLO_DECLARE_WAR_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',				),
-		-- Diplomacy Text: Make Peace
+-- Diplomacy Text: Make Peace
 		-- AI accepts from human
 		('en_US',	'LOC_DIPLO_MAKE_PEACE_AI_ACCEPT_DEAL_LEADER_BC_DUDEOBAMA_ANY',				),
 		-- AI refuses human
@@ -108,9 +119,11 @@ VALUES	('en_US',	'LOC_LEADER_BC_DUDEOBAMA_NAME',	'Dudeobama'),
 		('en_US',	'LOC_DIPLO_DEFEAT_FROM_AI_LEADER_BC_DUDEOBAMA_ANY',				),*/
 		-- Human is Defeated (will see this in hotseat)
 		('en_US',	'LOC_DIPLO_DEFEAT_FROM_HUMAN_LEADER_BC_DUDEOBAMA_ANY', '{LOC_DIPLO_DEFEAT_FROM_AI_LEADER_BC_DUDEOBAMA_ANY}'),
+
 		('en_US',	'LOC_PEDIA_LEADERS_PAGE_BC_DUDEOBAMA_QUOTE', 'In progress'),
 		('en_US',	'LOC_PEDIA_LEADERS_PAGE_BC_DUDEOBAMA_TITLE',	'In progress'	),
 		('en_US',	'LOC_PEDIA_LEADERS_PAGE_BC_DUDEOBAMA_SUBTITLE',	'In progress'			),
+
 		('en_US',	'LOC_PEDIA_LEADERS_PAGE_LEADER_BC_DUDEOBAMA_CHAPTER_CAPSULE_BODY', 'In progress'),
 		('en_US',	'LOC_PEDIA_LEADERS_PAGE_LEADER_BC_DUDEOBAMA_CHAPTER_DETAILED_BODY', 'In progress'),
 		('en_US',	'LOC_PEDIA_LEADERS_PAGE_LEADER_BC_DUDEOBAMA_CHAPTER_HISTORY_PARA_1', 'In progress');
